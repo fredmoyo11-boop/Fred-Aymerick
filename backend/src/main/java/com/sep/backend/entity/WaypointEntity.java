f2114585cee142b4377abc8e295490ef090fcb03
Ian Christopher Schirmer <ian.schirmer@stud.uni-due.de>
2025-04-29 12:55:14 +0200

    added Waypoint

---
+++ backend/src/main/java/com/sep/backend/entity/WaypointEntity.java
@@ -0,0 +1,41 @@
+package com.sep.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class WaypointEntity extends AbstractEntity {
+    @NotBlank
+    @Column(name = "index", unique = false, nullable = false)
+    private long index;
+
+    @NotBlank
+    @Column(name = "longitude", nullable = false)
+    private String longitude;
+
+    @NotBlank
+    @Column(name = "latitude", nullable = false)
+    private String latitude;
+
+//    @NotNull
+//    @Column(name = "verified", nullable = false)
+//    private Boolean verified;
+//
+//    @Column(name = "profile_picture_url")
+//    private String profilePictureUrl;
+
+//    public void setBirthday(String birthday) {
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//        this.birthday = LocalDate.parse(birthday, formatter);
+//    }
+}

d7d8decfab921807ae1918ad19f5e28449f4ddc2
Fred Aymerick Moyo <fred.moyo@stud.uni-duisburg-essen.de>
2025-05-08 22:17:43 +0200

    fügt  @Schema annotation bei  IsOwner -Annotation.

--- backend/src/main/java/com/sep/backend/account/IsOwner.java
+++ backend/src/main/java/com/sep/backend/account/IsOwner.java
@@ -1,5 +1,6 @@
 package com.sep.backend.account;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.lang.annotation.*;
@@ -7,6 +8,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @PreAuthorize("@accountService.isOwner(#username)")
 @Documented
+@Schema(description = "Checks if the current user is the owner of the account.")
 public @interface IsOwner {
 }
 

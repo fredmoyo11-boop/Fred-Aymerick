d603a9fc292c4da0b1839915cd404ad0c159c578
Reine90 <demgnereine08@gmail.com>
2025-05-11 22:42:23 +0200

    Fahranfrage-form layout gemacht

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {VerifyEmailComponent} from './components/verify-email/verify-email.compo
 import {LoginComponent} from './components/login/login.component';
 import {StandardLayoutComponent} from './layouts/standard-layout/standard-layout.component';
 import {HomeComponent} from './components/home/home.component';
+import {FahranfrageErstellenComponent} from './components/fahranfrage-erstellen/fahranfrage-erstellen.component';
 
 export const routes: Routes = [
   {path: "register", component: RegisterComponent},
@@ -16,6 +17,7 @@ export const routes: Routes = [
   {
     path: "", component: StandardLayoutComponent, children: [
       {path: "", component: HomeComponent},
+      {path: "fahranfrage", component: FahranfrageErstellenComponent},
     ]
   },
   {path: "**", redirectTo: ""}

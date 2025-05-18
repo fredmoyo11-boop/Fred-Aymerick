ac05294fda873ae8db64d38b6876bd66f22c3cc3
Mats Paul Tončik <mats.toncik@stud.uni-due.de>
2025-05-18 11:07:42 +0200

    Updated gitignore to ignore generated client.

--- frontend/src/api/sep_drive/model/models.ts
+++
@@ -1,7 +0,0 @@
-export * from './accountDTO';
-export * from './authResponse';
-export * from './loginRequest';
-export * from './otpRequest';
-export * from './rating';
-export * from './stringResponse';
-export * from './waypointResponse';

65d93e9aa28a802ab677da7468c945c120bcffde
Mats Paul Tončik <mats.toncik@stud.uni-due.de>
2025-05-18 18:25:44 +0200

    Updated route visualization logic.

--- frontend/src/app/components/map/map.component.ts
+++ frontend/src/app/components/map/map.component.ts
@@ -1,152 +1,121 @@
 import {Component, inject, OnInit} from '@angular/core';
 import * as L from 'leaflet';
 import 'leaflet-routing-machine';
-import {RouteService} from '../../../api/sep_drive';
+import {RouteService, WaypointResponse} from '../../../api/sep_drive';
 import {MatButton, MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
+import {CircleMarkerOptions, LatLngExpression} from "leaflet";
 
 @Component({
-  selector: 'app-map',
-  imports: [
-    MatButton,
-    MatIconButton,
-    MatIcon
-  ],
-  templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+    selector: 'app-map',
+    imports: [
+        MatButton,
+        MatIconButton,
+        MatIcon
+    ],
+    templateUrl: './map.component.html',
+    styleUrl: './map.component.css'
 })
 export class MapComponent implements OnInit {
-  routeService = inject(RouteService)
+    routeService = inject(RouteService)
 
-  index = 0
+    private map!: L.Map;
+    private routeLayer = L.layerGroup()
 
-  map!: L.Map;
-  routingControl: L.Routing.Control | undefined = undefined;
+    index = 0
+    selectedGeoJSONFile: File | null = null;
 
-  startLan!: number;
-  startLon!: number;
-  endLan!: number;
-  endLon!: number;
-  viaPoints: Array<{ lan: number; lon: number }> = [];
+    ngOnInit(): void {
+        this.map = L.map("map").setView([0, 0], 2);
 
-  ngOnInit(): void {
-    this.map = L.map("map");
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(this.map);
 
-    this.getRouteDataFromBackend(this.index);
-  }
-
-  selectedGeoJSONFile: File | null = null;
-
-  onFileChanged(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length) {
-      this.selectedGeoJSONFile = input.files[0];
-    }
-  }
-
-  public createRoute(): void {
-    this.routeService.importRoute(this.selectedGeoJSONFile!).subscribe({
-      next: (res) => {
-        this.index = Number(res.message);
-        this.getRouteDataFromBackend(this.index)
-      },
-      error: (err) => {
-        console.error(err);
-      }
-    })
-  }
-
-
-  private getRouteDataFromBackend(index: number): void {
-    this.routeService.getFullRoute(index).subscribe(data => {
-      if (data.length >= 2) {
-        const start = data[0];
-        const end = data[data.length - 1];
-
-        this.startLan = parseFloat(start.latitude);
-        this.startLon = parseFloat(start.longitude);
-
-        this.endLan = parseFloat(end.latitude);
-        this.endLon = parseFloat(end.longitude);
-
-        this.viaPoints = []
-      } else {
-        // fallback demonstration route
-        this.startLan = 51.4516; // Essen Hbf
-        this.startLon = 7.0146;
-
-        this.endLan = 51.5136;   // Dortmund Hbf
-        this.endLon = 7.4653;
-
-        this.viaPoints = [
-          {lan: 51.4800, lon: 7.2000},
-          {lan: 51.4900, lon: 7.3000}
-        ];
-      }
-      this.initMap();
-    });
-  }
-
-  private initMap(): void {
-    this.map.setView([51.505, -0.09], 13);
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(this.map);
-
-
-    const greenIcon = L.icon({
-      iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png',
-      iconSize: [40, 40],
-      iconAnchor: [20, 40],
-      popupAnchor: [0, -40]
-    });
-
-    const redIcon = L.icon({
-      iconUrl: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
-      iconSize: [40, 40],
-      iconAnchor: [20, 40],
-      popupAnchor: [0, -40]
-    });
-
-    const orangeIcon = L.icon({
-      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png',
-      iconSize: [25, 41],
-      iconAnchor: [20, 40],
-      popupAnchor: [0, -40]
-    });
-
-    if (this.routingControl) {
-      this.routingControl.setWaypoints([])
+        this.routeLayer.addTo(this.map);
     }
 
-    this.routingControl = (L as any).Routing.control({
-      waypoints: [
-        L.latLng(this.startLan, this.startLon),
-        ...this.viaPoints.map(p => L.latLng(p.lan, p.lon)),
-        L.latLng(this.endLan, this.endLon)
-      ],
-      routeWhileDragging: false,
-      lineOptions: {
-        styles: [
-          {color: '#1E90FF', opacity: 0.85, weight: 6, dashArray: '', lineCap: 'round'}
-        ]
-      },
-      addWaypoints: false,
-      fitSelectedRoutes: true,
-      show: true,
-      createMarker: (i: number, wp: any, nWps: number) => {
-        if (i === 0) {
-          return L.marker(wp.latLng, {icon: greenIcon}).bindPopup('Startpunkt');
-        } else if (i === nWps - 1) {
-          return L.marker(wp.latLng, {icon: redIcon}).bindPopup('Endpunkt');
-        } else {
-          return L.marker(wp.latLng, {icon: orangeIcon}).bindPopup(`Zwischenstopp ${i}`);
+
+    onFileChanged(event: Event) {
+        const input = event.target as HTMLInputElement;
+        if (input.files && input.files.length) {
+            this.selectedGeoJSONFile = input.files[0];
         }
-      }
-    }).addTo(this.map);
-  }
+    }
+
+    public createRoute(): void {
+        this.routeService.importRoute(this.selectedGeoJSONFile!).subscribe({
+            next: (res) => {
+                this.index = Number(res.message);
+                this.getRouteDataFromBackend(this.index)
+            },
+            error: (err) => {
+                console.error(err);
+            }
+        })
+    }
+
+
+    private getRouteDataFromBackend(index: number): void {
+        this.routeService.getFullRoute(index).subscribe(data => {
+            this.updateMap(data)
+        });
+    }
+
+    private updateMap(data: WaypointResponse[]): void {
+        let stopMarkerOptions: CircleMarkerOptions = {
+            radius: 5,
+            fillColor: "#ff7800",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        }
+
+        let startMarkerOptions: CircleMarkerOptions = {
+            radius: 8,
+            fillColor: "#0db610",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        }
+
+        let endMarkerOptions: CircleMarkerOptions = {
+            radius: 8,
+            fillColor: "#df081f",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        }
+
+        this.routeLayer.clearLayers()
+
+        const coordinates: LatLngExpression[] = data.map(wp => [Number(wp.latitude), Number(wp.longitude)]).slice(1, -1) as LatLngExpression[];
+
+        L.polyline(coordinates, {color: "blue"}).addTo(this.routeLayer);
+
+        const start = coordinates[0]
+        L.circleMarker(start, startMarkerOptions).addTo(this.routeLayer);
+
+        const end = coordinates[coordinates.length - 1]
+        L.circleMarker(end, endMarkerOptions).addTo(this.routeLayer);
+
+        // choose random point from coordinates
+        const randomPoint = (points: LatLngExpression[]) => {
+            const idx = Math.floor(Math.random() * points.length)
+            return points[idx]
+        }
+
+        // for demonstration add between 0 and 5 random stop points to the route
+        for (let i = 0; i < Math.floor(Math.random() * 6); i++) {
+            L.circleMarker(randomPoint(coordinates), stopMarkerOptions).addTo(this.routeLayer);
+        }
+
+        this.map.fitBounds([start as L.LatLngTuple, end as L.LatLngTuple])
+    }
 
 
 }

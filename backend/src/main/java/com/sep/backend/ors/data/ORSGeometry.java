bda42141e471acc4e0982f0af9e3593c88aeb3d3
Thomas Fabio Kaiser <thomas.kaiser@stud.uni-due.de>
2025-05-27 17:12:27 +0200

    Added ORS data structure

---
+++ backend/src/main/java/com/sep/backend/ors/data/ORSGeometry.java
@@ -0,0 +1,26 @@
+package com.sep.backend.ors.data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "Represents the GeoJSON geometry.")
+public class ORSGeometry {
+
+    @JsonProperty("type")
+    @Schema(description = "The geometry type.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String type;
+
+    @JsonProperty("coordinates")
+    @Schema(description = "The coordinates of the geometry.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private List<List<Double>> coordinates;
+}
---
+++ backend/src/main/java/com/sep/backend/ors/data/ORSStep.java
@@ -0,0 +1,40 @@
+package com.sep.backend.ors.data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "Represents a step.")
+public class ORSStep {
+
+    @JsonProperty("distance")
+    @Schema(description = "The distance.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private double distance;
+
+    @JsonProperty("duration")
+    @Schema(description = "The duration.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private double duration;
+
+    @JsonProperty("type")
+    @Schema(description = "The type.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private int type;
+
+    @JsonProperty("instruction")
+    @Schema(description = "The instruction.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String instruction;
+
+    @JsonProperty("name")
+    @Schema(description = "The name.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String name;
+
+    @JsonProperty("way_points")
+    @Schema(description = "The way points.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private int[] wayPoints;
+}

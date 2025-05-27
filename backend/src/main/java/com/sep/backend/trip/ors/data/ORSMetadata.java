d1621a75eb1bf325ad2ad747b050cb126a2f9b2e
Thomas Fabio Kaiser <thomas.kaiser@stud.uni-due.de>
2025-05-27 16:55:52 +0200

    Added ORS data structure

---
+++ backend/src/main/java/com/sep/backend/trip/ors/data/ORSMetadata.java
@@ -0,0 +1,28 @@
+package com.sep.backend.trip.ors.data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Represents the meta data produces by ORS.")
+public class ORSMetadata {
+
+    @JsonProperty("attribution")
+    @Schema(description = "The attribution.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String attribution;
+
+    @JsonProperty("service")
+    @Schema(description = "The service.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String service;
+
+    @JsonProperty("timestamp")
+    @Schema(description = "The timestamp.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private long timestamp;
+
+    @JsonProperty("query")
+    @Schema(description = "The query.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private ORSQuery query;
+
+    @JsonProperty("engine")
+    @Schema(description = "The engine.", requiredMode = Schema.RequiredMode.REQUIRED)
+    private ORSEngine engine;
+}

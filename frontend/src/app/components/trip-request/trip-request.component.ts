6066c668381f4873cc8aa3c9cda2f124e146adf4
Thomas Fabio Kaiser <thomas.kaiser@stud.uni-due.de>
2025-06-06 00:28:08 +0200

    Started working on frontend trip request

--- frontend/src/app/components/trip-request/trip-request.component.ts
+++ frontend/src/app/components/trip-request/trip-request.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+
+import {
+  Location,
+  ORSFeatureCollection,
+  RouteDTO,
+  RouteService,
+  TripRequestBody,
+  TripRequestDTO,
+  TripRequestService
+} from '../../../api/sep_drive';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-trip-request',
@@ -6,6 +17,43 @@ import { Component } from '@angular/core';
   templateUrl: './trip-request.component.html',
   styleUrl: './trip-request.component.css'
 })
-export class TripRequestComponent {
+export class TripRequestComponent implements OnInit{
+  tripRequestService = inject(TripRequestService)
+  routeService = inject(RouteService)
 
+  tripRequestDTO: TripRequestDTO | null = null
+
+  routeDTO: RouteDTO | null = null
+  orsFeatureCollection: ORSFeatureCollection | null = null
+
+  stops: Location[] = []
+
+  distance: number = 0
+  duration: number = 0
+  note: string = ""
+
+  form = new FormGroup({
+    carType: new FormControl('SMALL')
+  })
+
+
+
+  ngOnInit(): void {
+
+  }
+
+
+  createTripRequest(): void {
+    const tripRequestBody : TripRequestBody = {
+      carType: this.form.value.carType!,
+      geojson: this.orsFeatureCollection!,
+      locations: this.stops,
+      note: this.note
+    }
+    this.tripRequestService.createCurrentActiveTripRequest(tripRequestBody).subscribe({
+      next: tripRequest => {
+
+      }
+    })
+  }
 }
